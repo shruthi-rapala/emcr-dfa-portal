62f464c09f70e50ebb28d9589f65b1d173f53aec
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2023-09-01 14:56:45 -0700

    comma to yes but

--- dfa/src/UI/embc-dfa/src/app/core/api/models/insurance-option.ts
+++ dfa/src/UI/embc-dfa/src/app/core/api/models/insurance-option.ts
@@ -6,6 +6,6 @@
  */
 export enum InsuranceOption {
   Yes = 'Yes, my insurance will cover all my losses.',
-  Unsure = 'Yes but I don\'t know if my insurance will cover all damages or for this event.',
+  Unsure = 'Yes, but I don\'t know if my insurance will cover all damages or for this event.',
   No = 'No'
 }
