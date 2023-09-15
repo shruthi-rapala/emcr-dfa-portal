1f955a9fec68b325ccd40f11947665fed98a4552
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2023-09-15 09:16:03 -0700

    required doc type

--- dfa/src/UI/embc-dfa/src/app/core/components/dfa-attachment/dfa-attachment.component.ts
+++ dfa/src/UI/embc-dfa/src/app/core/components/dfa-attachment/dfa-attachment.component.ts
@@ -74,53 +74,12 @@ export class DfaAttachmentComponent implements OnInit, OnDestroy {
       this.fileUpload.get('modifiedBy').setValue("Applicant");
       if (this.fileType) this.fileUpload.get('fileType').setValue(this.fileType); else this.fileUpload.get('fileType').setValue(null);
       if (this.requiredDocumentType) this.fileUpload.get('requiredDocumentType').setValue(this.requiredDocumentType); else this.fileUpload.get('requiredDocumentType').setValue(null);
-      // this.fileUpload.get('addNewFileUploadIndicator').setValue(true);
       this.fileUpload.get('deleteFlag').setValue(false);
       this.fileUpload.get('applicationId').setValue(this.dfaApplicationMainDataService.getApplicationId());
       this.fileUpload.get('id').setValue(null);
       this.fileUpload.updateValueAndValidity();
     }
   }
-
-  showForm() {
-    console.log(this.fileUpload);
-  }
-
-  // initIdentification() {
-  //   this.identificationForm$ = this.formCreationService
-  //   .getFileUploadsForm()
-  //   .subscribe((fileUploads) => {
-  //     this.identificationForm = fileUploads;
-  //     this.identificationForm.addValidators([this.validateFormIdentification]);
-  //   });
-
-  //   this.identificationForm
-  //     .get('addNewFileUploadIndicator')
-  //     .valueChanges.subscribe((value) => this.updateFileUploadFormOnVisibility(this.identificationForm.get('identificationFileUpload')));
-
-  //   const _identificationFormArray = this.formCreationService.fileUploadsForm.value.get('fileUploads');
-  //     _identificationFormArray.valueChanges
-  //       .pipe(
-  //         mapTo(_identificationFormArray.getRawValue())
-  //         ).subscribe(data => this.identificationDataSource.data = _identificationFormArray.getRawValue()?.filter(x => x.requiredDocumentType == Object.keys(this.RequiredDocumentTypes)[Object.values(this.RequiredDocumentTypes).indexOf(this.RequiredDocumentTypes.Identification)] && x.deleteFlag == false));
-
-    // initialize file upload form
-    // let fileUploads = this.formCreationService.fileUploadsForm.value.get('fileUploads').value;
-    // if (fileUploads?.filter(x => x.requiredDocumentType === Object.keys(this.RequiredDocumentTypes)[Object.values(this.RequiredDocumentTypes).indexOf(this.RequiredDocumentTypes.Identification)]).length > 0) {
-    //   let identificationFoundIndex = fileUploads.findIndex(x => x.requiredDocumentType === Object.keys(this.RequiredDocumentTypes)[Object.values(this.RequiredDocumentTypes).indexOf(this.RequiredDocumentTypes.Identification)]);
-    //   this.identificationForm.get('identificationFileUpload').setValue(fileUploads[identificationFoundIndex]);
-    // } else {
-      // this.identificationForm.get('identificationFileUpload').reset();
-      // this.identificationForm.get('identificationFileUpload.modifiedBy').setValue("Applicant");
-      // this.identificationForm.get('identificationFileUpload.fileType').setValue(Object.keys(this.FileCategories)[Object.values(this.FileCategories).indexOf(this.FileCategories.TenancyProof)]);
-      // this.identificationForm.get('identificationFileUpload.requiredDocumentType').setValue(Object.keys(this.RequiredDocumentTypes)[Object.values(this.RequiredDocumentTypes).indexOf(this.RequiredDocumentTypes.Identification)]);
-      // this.identificationForm.get('addNewFileUploadIndicator').setValue(true);
-      // this.identificationForm.get('identificationFileUpload.deleteFlag').setValue(false);
-      // this.identificationForm.get('identificationFileUpload.applicationId').setValue(this.dfaApplicationMainDataService.getApplicationId());
-      // this.identificationForm.get('identificationFileUpload.id').setValue(null);
-    // }
-    // this.identificationForm.updateValueAndValidity();
-  // }
 
   // Preserve original property order
   originalOrder = (a: KeyValue<number,string>, b: KeyValue<number,string>): number => {
