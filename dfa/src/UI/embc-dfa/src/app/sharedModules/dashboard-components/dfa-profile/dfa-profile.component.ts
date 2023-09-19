c2c8dbf071e0253a8b5ffe18239f7e93d28a3d99
AMERICAS\311816 <prabin.sebastian@nttdata.com>
2023-09-19 08:27:44 -0700

    EMBCDFA-929

--- dfa/src/UI/embc-dfa/src/app/sharedModules/dashboard-components/dfa-profile/dfa-profile.component.ts
+++ dfa/src/UI/embc-dfa/src/app/sharedModules/dashboard-components/dfa-profile/dfa-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ApplicationService as Service } from '../../../core/api/services/application.service';
+import { AppSessionService } from 'src/app/core/services/appSession.service';
 
 @Component({
   selector: 'app-dfa-profile',
@@ -14,12 +16,28 @@ export class DfaProfileComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private appService: Service,
+    private appSessionService: AppSessionService,
   ) {
   }
 
   ngOnInit(): void {
     this.currentFlow = this.route.snapshot.data.flow;
+
+    this.appService.applicationGetDfaApplications().subscribe({
+      next: (lstData) => {
+        if (lstData != null) {
+          this.mapData(lstData);
+        }
+      },
+      error: (error) => {
+      }
+    });
   }
 
+  mapData(lstApp: Object): void {
+    var res = JSON.parse(JSON.stringify(lstApp));
+    this.appSessionService.appNumber = res.length.toString() != null ? res.length.toString() : "0";
+  }
 }
