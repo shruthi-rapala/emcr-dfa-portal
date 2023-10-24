76f9875d5cb495ff12fa77e63ff1bf65db784d43
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2023-10-24 16:28:58 -0700

    disable form controls

--- dfa/src/UI/embc-dfa/src/app/feature-components/dashboard/dashboard.component.ts
+++ dfa/src/UI/embc-dfa/src/app/feature-components/dashboard/dashboard.component.ts
@@ -115,9 +115,9 @@ export class DashboardComponent implements OnInit {
     let lstApplications = res;
     this.currentApplicationsCount = 0; this.pastApplicationsCount = 0;
     lstApplications.forEach(x => {
-      if ((x.status.toLowerCase() === "dfa decision made"
-        || x.status.toLowerCase() === "closed: inactive" || x.status.toLowerCase() === "closed: withdrawn")
-        && (x.dateFileClosed && (this.sixtyOneDaysAgo <= new Date(x.dateFileClosed).getDate()))) { // TODO: uncomment
+      if ((x.status.toLowerCase() === "dfa decision made" || x.status.toLowerCase() == "reviewing application"
+        || x.status.toLowerCase() === "closed: inactive" || x.status.toLowerCase() === "closed: withdrawn")) {
+        // && (x.dateFileClosed && (this.sixtyOneDaysAgo <= new Date(x.dateFileClosed).getDate()))) {
           this.pastApplicationsCount++;
       } else this.currentApplicationsCount++;
     })
