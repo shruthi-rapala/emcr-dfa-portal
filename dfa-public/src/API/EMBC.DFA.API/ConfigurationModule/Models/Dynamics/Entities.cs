8edea2b1bce1a916ea8f9164af87249e590e74a0
ksprabin <ksprabin@gmail.com>
2024-06-24 14:44:38 -0700

    EMCRI-197

--- dfa-public/src/API/EMBC.DFA.API/ConfigurationModule/Models/Dynamics/Entities.cs
+++ dfa-public/src/API/EMBC.DFA.API/ConfigurationModule/Models/Dynamics/Entities.cs
@@ -423,6 +423,7 @@ namespace EMBC.DFA.API.ConfigurationModule.Models.Dynamics
         public string? dfa_projectbusinessprocessstages { get; set; }
         public DateTime? dfa_estimatedcompletiondateofproject { get; set; }
         public int? dfa_estimatedcost { get; set; }
+        public string? dfa_projectbusinessprocesssubstages { get; set; }
         public bool? dfa_dateofdamagesameasapplication { get; set; }
     }
 
