4aa73d0b2934885ed5f323b1a281bdf9f8700e5c
Wayne Peters <Wayne.Peters@gov.bc.ca>
2024-08-22 14:19:46 -0700

    [EMCRI] Work in progress for new Create Application screens

--- dfa-public/src/UI/embc-dfa/src/app/sharedModules/forms/dfa-application-main-forms/create-application1/create-application1.component.ts
+++ dfa-public/src/UI/embc-dfa/src/app/sharedModules/forms/dfa-application-main-forms/create-application1/create-application1.component.ts
@@ -1,50 +1,66 @@
+import { Component, OnInit, NgModule, Inject, OnDestroy } from '@angular/core';
 import {
   UntypedFormBuilder,
   UntypedFormGroup,
   AbstractControl,
   FormsModule,
-  Validators
+  Validators,
+  FormGroup,
+  FormControl
 } from '@angular/forms';
-import { Component, Inject, Input, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { CommonModule, KeyValue } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormCreationService } from 'src/app/core/services/formCreation.service';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { DirectivesModule } from '../../../../core/directives/directives.module';
 import { CustomValidationService } from 'src/app/core/services/customValidation.service';
+import { distinctUntilChanged } from 'rxjs/operators';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
 import { DFAApplicationMainDataService } from 'src/app/feature-components/dfa-application-main/dfa-application-main-data.service';
+import { ApplicantOption, ApplicantSubtypeSubCategories } from 'src/app/core/api/models';
+import { MatTableModule } from '@angular/material/table';
+import { CustomPipeModule } from 'src/app/core/pipe/customPipe.module';
+import { DFADeleteConfirmDialogComponent } from '../../../../core/components/dialog-components/dfa-confirm-delete-dialog/dfa-confirm-delete.component';
 import { MatDialog } from '@angular/material/dialog';
+import { NgxMaskDirective, NgxMaskPipe, NgxMaskService, provideNgxMask } from 'ngx-mask';
+import { ApplicationService, OtherContactService } from 'src/app/core/api/services';
 import { DFAApplicationMainMappingService } from 'src/app/feature-components/dfa-application-main/dfa-application-main-mapping.service';
-import { ApplicationService } from 'src/app/core/api/services';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-create-application1',
-  standalone: true,
-  imports: [],
   templateUrl: './create-application1.component.html',
   styleUrl: './create-application1.component.scss'
 })
-export default class CreateApplication1Component implements OnInit {
-  @Input() createApplication1Form: UntypedFormGroup;
-  // formBuilder: UntypedFormBuilder;
-  // propertyDamageForm$: Subscription;
-  // formCreationService: FormCreationService;
-  // isReadOnly: boolean;
-  // vieworedit: string;
+export class CreateApplication1Component implements OnInit, OnDestroy {
+  // createApplication1Form = new FormGroup({
+  //   legalName: new FormControl('')
+  // });
+  formBuilder: UntypedFormBuilder;
+  createApplication1Form: UntypedFormGroup;
+  createApplication1Form$: Subscription;
+  formCreationService: FormCreationService;
+  isReadOnly: boolean;
+  vieworedit: string;
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
-/*
   constructor(
-    // @Inject('formBuilder') formBuilder: UntypedFormBuilder,
-    // @Inject('formCreationService') formCreationService: FormCreationService,
+    @Inject('formBuilder') formBuilder: UntypedFormBuilder,
+    @Inject('formCreationService') formCreationService: FormCreationService,
     public customValidator: CustomValidationService,
     public dfaApplicationMainDataService: DFAApplicationMainDataService,
     private applicationService: ApplicationService,
     private dfaApplicationMainMapping: DFAApplicationMainMappingService,
     public dialog: MatDialog
   ) {
-    // this.formBuilder = formBuilder;
-    // this.formCreationService = formCreationService;
+    this.formBuilder = formBuilder;
+    this.formCreationService = formCreationService;
     this.isReadOnly = (dfaApplicationMainDataService.getViewOrEdit() === 'view'
     || dfaApplicationMainDataService.getViewOrEdit() === 'edit'
     || dfaApplicationMainDataService.getViewOrEdit() === 'viewOnly');
@@ -63,6 +79,7 @@ export default class CreateApplication1Component implements OnInit {
   setViewOrEditControls() {
     if (!this.createApplication1Form) return;
     if (this.isReadOnly) {
+
       this.createApplication1Form.controls.legalName.disable();
     } else {
       this.createApplication1Form.controls.legalName.enable();
@@ -71,7 +88,7 @@ export default class CreateApplication1Component implements OnInit {
   }
 
   ngOnInit(): void {
-    this.propertyDamageForm$ = this.formCreationService
+    this.createApplication1Form$ = this.formCreationService
       .getCreateApplication1Form()
       .subscribe((createApplication1) => {
         this.createApplication1Form = createApplication1;
@@ -94,10 +111,10 @@ export default class CreateApplication1Component implements OnInit {
   get createApplication1FormControl(): { [key: string]: AbstractControl } {
     return this.createApplication1Form.controls;
   }
-*/
 
 
-  // ngOnDestroy(): void {
-  //   this.propertyDamageForm$.unsubscribe();
-  // }
+  ngOnDestroy(): void {
+    this.createApplication1Form$.unsubscribe();
+  }
 }
+
