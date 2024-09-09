9ef8b9a1d7043a0044adb32080470325ade5640d
Karim Hassanin <271015@NTTDATA.COM>
2024-09-09 14:06:25 -0700

    EMCRI-564 Creating a PDF service to generate PDF

---
+++ pdf-service/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿
+namespace pdfservice.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BusinessPhone { get; set; }
+        public string Email { get; set; }
+        public string CellPhone { get; set; }
+        public string JobTitle { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
