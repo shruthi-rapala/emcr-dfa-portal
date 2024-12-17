9315708b230340bce100f865ccff3c9b067fcb58
Sam Warren <samwarren4@gmail.com>
2024-12-17 13:27:06 -0800

    dfa-feature-flags: remove library

--- dfa/src/API/EMBC.DFA.API/Configuration.cs
+++ dfa/src/API/EMBC.DFA.API/Configuration.cs
@@ -17,7 +17,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 using NSwag;
