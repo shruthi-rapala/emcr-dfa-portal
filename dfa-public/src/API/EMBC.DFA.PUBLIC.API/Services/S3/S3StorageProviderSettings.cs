09f3049098a3f8bb4399082a710bf53d15d0449c
George Walker <george@walkersoftware.ca>
2025-03-03 13:47:35 -0800

    Initial commit for Files API

---
+++ dfa-public/src/API/EMBC.DFA.PUBLIC.API/Services/S3/S3StorageProviderSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace EMBC.DFA.PUBLIC.API.Services.S3
+{
+    public class S3StorageProviderSettings
+    {
+        public string Url { get; set; } = null!;
+        public string AccessKey { get; set; } = null!;
+        public string SecretKey { get; set; } = null!;
+        public string BucketName { get; set; } = null!;
+    }
+}
