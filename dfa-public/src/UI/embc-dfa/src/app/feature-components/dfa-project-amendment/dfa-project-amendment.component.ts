862946d93322ddb706203adb823899d9eaf28396
shruthi-rapala <shruthi.rapala@quartech.com>
2025-03-04 14:32:12 -0800

    EMCR # 1014 Portal: On Project Amendment form, don't display values in certain fields until project amendment has been approved or approved with exceptions  Fixed the issue with the amendment approved date

--- dfa-public/src/UI/embc-dfa/src/app/feature-components/dfa-project-amendment/dfa-project-amendment.component.ts
+++ dfa-public/src/UI/embc-dfa/src/app/feature-components/dfa-project-amendment/dfa-project-amendment.component.ts
@@ -318,7 +318,7 @@ export class DFAProjectAmendmentComponent
       this.statusBar = JSON.parse(jsonVal);
 
       if(objAmendment.amendmentDecision !== "Approved" && objAmendment.amendmentDecision !== "Approved with Exclusions") {
-        objAmendment.amendmentReceivedDate = null
+        objAmendment.amendmentApprovedDate = null
         objAmendment.emcrDecisionComments = null
         objAmendment.deadlineExtensionApproved = null
         objAmendment.amended18MonthDeadline = null
