274b9e7d9b416a32616f5a5300809c7934c8f8e1
JustinMacaulay <justinkmacaulay@gmail.com>
2025-03-17 13:27:17 -0700

    missed project types (#793)

---
+++ dfa-public/src/UI/embc-dfa/src/app/core/api/models/project-types.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export enum ProjectTypes {
+  InfrastructureRepair = 'InfrastructureRepair',
+  DebrisCleanup = 'DebrisCleanup',
+  Engineering = 'Engineering',
+  PlanningDesign = 'PlanningDesign',
+  Other = 'Other'
+}
