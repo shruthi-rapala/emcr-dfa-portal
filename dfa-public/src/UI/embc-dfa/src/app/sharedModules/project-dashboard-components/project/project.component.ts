e4d3673adab91f0ba9aad148e11b4f94c7486d0e
shruthi-rapala <104577563+shruthi-rapala@users.noreply.github.com>
2025-05-22 15:29:14 -0700

    DFA # 33 RAFT: Projects disappear if DFA user clears project decision from Decision Made. (#843)
    * D4P # 50 Portal: Delete option is not available to the invoice attachments for the claims in draft.
    - Fixed the issue with supported documents are not being uploaded
    
    * D4P# 54 **Landing page - Toggle on / off by Private / public event**
    
    - Fixed the logo for the Local Authorities and Indigenous Governing Bodies
    - Fixed the Spacing and uppercase/lowercase edits in section above the footer (“Contact Us” section)
    - fixed the BC Gov Logo move to right
    - Fixed Spacing issue in the Private / Public sections (so text/sections/buttons align across the two)
    - Fixed the info icon in the Private / Public sections
    - Fixed the Header updates (font, spacing, addition of second yellow line)
    
    * Fixed the issue with contact icon on landing page
    
    * Fixed the date formats for outage banner
    
    * D4P # 50 Portal: Delete option is not available to the invoice attachment
    Fixed the issue with the the supporting documents file description
    
    * Fixed spacing issue on outage banner
    
    * Fixed the issue with the BCeid Login text
    
    * Fixed the Charitable/Volunteer Organizations wording on landing page
    
    * Fixed outage banner styling issues
    
    * Fixed the banner condition
    
    * D4P # 54 Landing page - Toggle on / off by Private / public event
    Added Missing BC gov logo
    
    * D4P# 94 PublicPortal - The upload session should reset for a new document
    - Fixed the issue with the resetting the document fields after uploading on claims and projects
    
    * MER # 113
    AGINC0169206 - Portal Service Account created a case in Mercury for Remedial
    Fixed the issue with null exception on the stage in project dashboard

--- dfa-public/src/UI/embc-dfa/src/app/sharedModules/project-dashboard-components/project/project.component.ts
+++ dfa-public/src/UI/embc-dfa/src/app/sharedModules/project-dashboard-components/project/project.component.ts
@@ -115,11 +115,11 @@ export class DfaDashProjectComponent implements OnInit {
                   objApp.statusColor = '#E25E63';
                 }
 
-                if (objApp.status.toLowerCase() == 'draft') {
+                if (objApp.status?.toLowerCase() == 'draft') {
                   objApp.statusColor = '#639DD4';
                 }
 
-                if (objApp.status.toLowerCase().indexOf('decision made') > -1 && objApp.stage.toLowerCase().indexOf('progress') > -1) {
+                if (objApp.status?.toLowerCase().indexOf('decision made') > -1 && objApp.stage?.toLowerCase().indexOf('progress') > -1) {
                   objApp.statusColor = '#FDCB52';
                 }
               }
