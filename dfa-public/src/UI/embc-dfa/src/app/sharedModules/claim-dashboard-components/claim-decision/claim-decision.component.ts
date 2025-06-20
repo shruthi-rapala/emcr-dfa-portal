ec8bf602cf2fe3b5a717a63f847eabbf918c8116
shruthi-rapala <shruthi.rapala@quartech.com>
2025-06-20 11:01:33 -0700

    D4P # 211 Portal - Public Claim Appeals - New Claim Decision Screen - Added invoice table section on claim decision componnet - Added backend logic to wireup the table and show invoice data - Updated styles for the invoice table

--- dfa-public/src/UI/embc-dfa/src/app/sharedModules/claim-dashboard-components/claim-decision/claim-decision.component.ts
+++ dfa-public/src/UI/embc-dfa/src/app/sharedModules/claim-dashboard-components/claim-decision/claim-decision.component.ts
@@ -1,37 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
-import { DfaClaimMain } from 'src/app/core/api/models';
-import { ClaimService } from 'src/app/core/api/services';
+import { DfaClaimMain, Invoice } from 'src/app/core/api/models';
+import { ClaimService, InvoiceService } from 'src/app/core/api/services';
 import { CoreModule } from 'src/app/core/core.module';
+import { FormCreationService } from 'src/app/core/services/formCreation.service';
+import { DFAClaimMainDataService } from 'src/app/feature-components/dfa-claim-main/dfa-claim-main-data.service';
 import { DFAClaimMainMappingService } from 'src/app/feature-components/dfa-claim-main/dfa-claim-main-mapping.service';
 @Component({
   selector: 'app-claim-decision',
   standalone: true,
-  imports: [CoreModule, MatCardModule],
+  imports: [CoreModule, MatCardModule, MatTableModule, CommonModule,],
   templateUrl: './claim-decision.component.html',
-  styleUrl: './claim-decision.component.scss'
+  styleUrl: './claim-decision.component.scss',
 })
 export class ClaimDecisionComponent implements OnInit {
 
   recoveryClaim?: DfaClaimMain;
-     private dfaClaimMainMapping: DFAClaimMainMappingService
-
   recoveryClaimFormAbstract: [];
+
+  documentSummaryColumnsToDisplay = ['invoiceNumber', 'vendorName', 'invoiceDate', 'totalBeingClaimed', 'invoiceAmount', 'emcrApprovedAmount'];
+  documentSummaryDataSource = new MatTableDataSource<InvoiceExtended>();
+  documentSummaryDataSourceFiltered = new MatTableDataSource<InvoiceExtended>();
+  invoicesCount: number = 0;
+  formCreationService: FormCreationService;
+
+
+
   constructor(
-     private claimService: ClaimService,
-     private route: ActivatedRoute,
-  ) {}
+    private claimService: ClaimService,
+    private route: ActivatedRoute,
+    public dfaClaimMainDataService: DFAClaimMainDataService,
+    private invoiceService: InvoiceService,
+    
+  ) { }
 
   ngOnInit(): void {
     var claimId = this.route.snapshot.paramMap.get('id');
     console.log('Claim ID from route:', claimId);
-    if (claimId) {  
-    this.getRecoveryClaim(claimId);
-    console.log('recoveryClaim?.id:', this.recoveryClaim?.id);
+    if (claimId) {
+      this.getRecoveryClaim(claimId);
     }
-    console.log('Claim Decision Component Initialized');
-  
+
+    // Get Recovery Claim Invoices
+    if( claimId) {
+      this.getRecoveryInvoices(claimId);
+    }
+
   }
 
 
@@ -43,11 +60,69 @@ export class ClaimDecisionComponent implements OnInit {
           //this.dfaClaimMainMapping.mapDFAClaimMain(dfaClaimMain);
           console.log('Recovery Claim:', this.recoveryClaim);
         },
-        error: (_error) => {}
+        error: (_error) => { }
       });
     }
   }
 
-  BackToDashboard(){
+  getRecoveryInvoices(claimId: string) {
+    if (claimId) {
+      this.invoiceService.invoiceGetDfaInvoices({ claimId: claimId }).subscribe({
+        next: (lstInv) => {
+          var lstInvoices = [];
+
+          lstInv.forEach((objInv) => {
+            lstInvoices.push({
+              invoiceId: objInv.invoiceId,
+              invoiceNumber: objInv.invoiceNumber,
+              vendorName: objInv.vendorName,
+              invoiceDate: new Date(objInv.invoiceDate),
+              isGoodsReceivedonInvoiceDate: objInv.isGoodsReceivedonInvoiceDate,
+              goodsReceivedDate: objInv.goodsReceivedDate
+                ? new Date(objInv.goodsReceivedDate)
+                : objInv.goodsReceivedDate,
+              purposeOfGoodsServiceReceived: objInv.purposeOfGoodsServiceReceived,
+              isClaimforPartofTotalInvoice: objInv.isClaimforPartofTotalInvoice,
+              reasonClaimingPartofTotalInvoice: objInv.reasonClaimingPartofTotalInvoice,
+              netInvoiceBeingClaimed: objInv.netInvoiceBeingClaimed,
+              pst: objInv.pst,
+              grossGST: objInv.grossGST,
+              actualInvoiceTotal: objInv.actualInvoiceTotal,
+              eligibleGST: objInv.eligibleGST,
+              totalBeingClaimed: objInv.totalBeingClaimed,
+              emcrDecision: objInv.emcrDecision,
+              emcrApprovedAmount: objInv.emcrApprovedAmount,
+              decisionDate: objInv.decisionDate ? new Date(objInv.decisionDate) : objInv.decisionDate,
+              emcrDecisionComments: objInv.emcrDecisionComments
+            });
+          });
+
+          this.documentSummaryDataSource.data = lstInvoices;
+          this.documentSummaryDataSourceFiltered.data = this.documentSummaryDataSource.data;
+          this.invoicesCount = this.documentSummaryDataSource.data.length;
+
+          // this.formCreationService.recoveryClaimForm.value
+          //   .get('invoices')
+          //   .setValue(this.documentSummaryDataSource.data);
+          // this.formCreationService.recoveryClaimForm.value.updateValueAndValidity();
+
+          // if (this.dfaClaimMainDataService.recoveryClaim) {
+          //   this.dfaClaimMainDataService.recoveryClaim.invoices = this.documentSummaryDataSource.data;
+          //   this.SummaryClaimCalc();
+          // }
+        },
+        error: (_error) => { }
+      });
+    }
   }
+
+  BackToDashboard() {
+  }
+
+
 }
+
+export interface InvoiceExtended extends Invoice {
+  invoiceId?: string;
+}
+
