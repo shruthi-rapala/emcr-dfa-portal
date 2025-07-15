19a1ae0f9480305390fa3b8ea991b97336f96600
shruthi-rapala <shruthi.rapala@quartech.com>
2025-07-15 10:41:23 -0700

    Cleanup  the commented code

--- dfa/src/UI/embc-dfa/src/app/feature-components/review/review.component.ts
+++ dfa/src/UI/embc-dfa/src/app/feature-components/review/review.component.ts
@@ -136,7 +136,6 @@ export class ReviewComponent implements OnInit {
     // subscribe to changes in other contacts
     const form = this.formCreationService.otherContactsForm.value;
     const otherContactsArray = form?.get('otherContacts') as FormArray;
-    // if (otherContactsArray) {
       otherContactsArray.valueChanges
         .pipe(
           startWith(otherContactsArray.value), // emit initial value immediately
@@ -145,8 +144,6 @@ export class ReviewComponent implements OnInit {
         .subscribe(data => {
           this.otherContactsDataSource.data = data;
         });
-    // }
-  
 
     // subscribe to changes in clean up logs
     const _cleanUpWorkFormArray = this.formCreationService.cleanUpLogItemsForm.value.get('cleanuplogs');
