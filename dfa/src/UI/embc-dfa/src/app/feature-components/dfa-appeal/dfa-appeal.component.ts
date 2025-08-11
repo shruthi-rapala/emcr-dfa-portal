f86e716f41e6cf1f9d431ac3d0721336a5effe29
shruthi-rapala <shruthi.rapala@quartech.com>
2025-08-11 08:50:22 -0700

    D4P # 28 PORTAL - Private Appeals - Appeal document uploading and tagging - Added the confirmation snackbar after the document is submitted.

--- dfa/src/UI/embc-dfa/src/app/feature-components/dfa-appeal/dfa-appeal.component.ts
+++ dfa/src/UI/embc-dfa/src/app/feature-components/dfa-appeal/dfa-appeal.component.ts
@@ -482,6 +482,18 @@ export class DfaAppealComponent implements OnInit {
             })
           )
           .subscribe({
+            next:() =>
+              {
+                this.snackBar.open(
+                  'Documents have been uploaded successfully',
+                  'Close',
+                  {
+                    horizontalPosition: 'center',
+                    verticalPosition: 'top',
+                    duration: 5000
+                  }
+                );
+              },
 
             complete: () => {
               this.isLoading = false;
