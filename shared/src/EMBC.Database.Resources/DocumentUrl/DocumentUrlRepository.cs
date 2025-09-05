47c97ece7e1d9a2cfb070c7e63c3d779eec56d34
Chris Tihor <64799899+Christopher-Tihor@users.noreply.github.com>
2025-09-05 11:04:01 -0700

    D4P-279: Fixed issue where all uploaded files were being displayed instead of just portal uploads. (#1002)

--- shared/src/EMBC.Database.Resources/DocumentUrl/DocumentUrlRepository.cs
+++ shared/src/EMBC.Database.Resources/DocumentUrl/DocumentUrlRepository.cs
@@ -1,4 +1,4 @@
-namespace EMBC.Database.Resources;
+﻿namespace EMBC.Database.Resources;
 
 public interface IDocumentUrlRepository : IBaseRepository<DocumentUrl>
 {
@@ -53,7 +53,9 @@ public class DocumentUrlRepository : BaseRepository<BcGoV_DocumentUrl, DocumentU
     {
         return _databaseContext
             .CreateQuery<BcGoV_DocumentUrl>()
-            .Where(query => query.DFA_AppealId != null && query.DFA_AppealId.Id == appealId)
+            .Where(query => query.DFA_AppealId != null && 
+                            query.DFA_AppealId.Id == appealId && 
+                            query.BcGoV_OriginCode == BcGoV_OriginCode.Portal) /* Get only the records uploaded by the portal. */
             .Select(result => _mapper.Map<DocumentUrl>(result))
             .ToList();
     }
