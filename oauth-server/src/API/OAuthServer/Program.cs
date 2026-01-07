238b37e7e9de5a9f68ffbafff5158f3d19025f3f
George Walker <george@walkersoftware.ca>
2026-01-06 16:11:08 -0800

    Ensure database is deleted before migration

--- oauth-server/src/API/OAuthServer/Program.cs
+++ oauth-server/src/API/OAuthServer/Program.cs
@@ -61,6 +61,7 @@ namespace OAuthServer
             var ctx = scope.ServiceProvider.GetService<PersistedGrantDbContext>();
             if (ctx == null) return;
             logger.LogInformation("Migrating PersistedGrantDbContext");
+            ctx.Database.EnsureDeleted(); 
             ctx.Database.Migrate();
             logger.LogInformation("PersistedGrantDbContext migration completed");
         }
